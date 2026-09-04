1701eb17bfd7805e5ef020eca3b06a1e98b27d77
ci7lus <7887955+ci7lus@users.noreply.github.com>
2026-09-05 01:38:01 +0900

    Update ref (contrib/ffmpeg: Fix MMT/TLV seeking flag)

--- compileAndBuildVLCKit.sh
+++ compileAndBuildVLCKit.sh
@@ -31,7 +31,7 @@ if [ -z "$MAKEFLAGS" ]; then
 fi
 
 BRANCH="master20260717"
-TESTEDHASH="ba4a3b20b1db034e46958238ec119a2cd1f90e7e" # libvlc hash that this version of VLCKit is build on
+TESTEDHASH="042cec3844373be1dbf8f46a1801499c1eb92497" # libvlc hash that this version of VLCKit is build on
 
 usage()
 {
